d8bcd6357a7c639f7c53abba02cdd0f5deaa286a
mtruong727 <125338662+mtruong727@users.noreply.github.com>
2025-01-08 15:14:23 -0600

    update event

--- SDLProject/MainSource.cpp
+++ SDLProject/MainSource.cpp
@@ -94,12 +94,28 @@ int main(int argc, char* args[])
 		}
 		else
 		{
-			//Apply the image
-			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
-			//Update the surface
-			SDL_UpdateWindowSurface( gWindow );
-			//Wait two seconds
-			SDL_Delay( 2000 );
+			//Main loop flag
+			bool quit = false;
+			//Event handler
+			SDL_Event e;
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+
+				//Apply the image
+				SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+				//Update the surface
+				SDL_UpdateWindowSurface(gWindow);
+			}
 		}
 	}
 	//Free resources and close SDL
