c4d9e355d3fe86724afbeec4b1dab0c129ea0177
mtruong727 <125338662+mtruong727@users.noreply.github.com>
2025-01-08 22:46:40 -0600

    Add Event handle

--- SDLProject/MainSource.cpp
+++ SDLProject/MainSource.cpp
@@ -1,10 +1,22 @@
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+#include <string>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+//Key press surfaces constants
+enum KeyPressSurfaces
+{
+	KEY_PRESS_SURFACE_DEFAULT, // 0
+	KEY_PRESS_SURFACE_UP, // 1
+	KEY_PRESS_SURFACE_DOWN, // 2
+	KEY_PRESS_SURFACE_LEFT, // 3
+	KEY_PRESS_SURFACE_RIGHT, // 4
+	KEY_PRESS_SURFACE_TOTAL // 5
+};
 
 //Starts up SDL and creates window
 bool init();
@@ -15,14 +27,22 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
+//Loads individual image
+//loadSurface function takes a string parameter and returns an SDL_Surface pointer
+//The function loads an image from a file at the specified path and returns the loaded image as an SDL_Surface
+SDL_Surface* loadSurface( std::string path );
+
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
 //The surface contained by the window
 SDL_Surface* gScreenSurface = NULL;
 
-//The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
+//The images that correspond to a keypress
+SDL_Surface* gKeyPressSurfaces[ KEY_PRESS_SURFACE_TOTAL ];
+
+//Current displayed image
+SDL_Surface* gCurrentSurface = NULL;
 
 bool init()
 {
@@ -78,6 +98,17 @@ void close()
 	SDL_Quit();
 }
 
+SDL_Surface* loadSurface(std::string path)
+{
+	//Load image at specified path
+	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+	}
+	return loadedSurface;
+}
+
 int main(int argc, char* args[])
 {
 	//Start up SDL and create window
@@ -99,13 +130,13 @@ int main(int argc, char* args[])
 			//Event handler
 			SDL_Event e;
 			//While application is running
-			while (!quit)
+			while ( !quit )
 			{
 				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
+				while (SDL_PollEvent( &e ) != 0 )
 				{
 					//User requests quit
-					if (e.type == SDL_QUIT)
+					if ( e.type == SDL_QUIT )
 					{
 						quit = true;
 					}
